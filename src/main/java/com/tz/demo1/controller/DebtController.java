ec9b928c61241a08df83d026dd340955f4027e22
taozhi <HP@tz>
2018-12-09 09:21:51 +0800

    初版

--- src/main/java/com/tz/demo1/controller/DebtController.java
+++ src/main/java/com/tz/demo1/controller/DebtController.java
@@ -14,25 +14,41 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.tz.demo1.service.CountService;
+
 @Controller
 public class DebtController {
-	@Autowired
-	private CountService service;
-	
-@GetMapping("/")
-	public String get(){
-		return "view";
-	}
+  @Autowired
+  private CountService service;
 
-@PostMapping("/")
-public void upload(@RequestParam("file")MultipartFile file,
-		RedirectAttributes redirectAttributes,@RequestParam("sex") String sex,@RequestParam("census") String census,
-		@RequestParam("low") String low,@RequestParam("high") String high,HttpServletResponse response) throws IOException{
+  @GetMapping("/")
+  public String get() {
+    return "view";
+  }
 
-double l =Double.parseDouble(low);
-double h=Double.parseDouble(high);
- 
-	service.deal(file,sex,census,l,h,response);
-	
-}
+  @PostMapping("/")
+  public void upload(@RequestParam("file") MultipartFile file, @RequestParam("sex") String sex,
+      @RequestParam("census") String census, @RequestParam("low") String low,
+      @RequestParam("high") String high, HttpServletResponse response,
+      @RequestParam("age") String age, @RequestParam("days") String days) throws IOException {
+
+    double l = Double.parseDouble(low);
+    double h = Double.parseDouble(high);
+    String census1 = null;
+    String age1 = null;
+
+    if (census.startsWith("全部")) {
+      census1 = census.substring(3);
+    } else {
+      census1 = census;
+    }
+
+    if (age.startsWith("全部")) {
+      age1 = age.substring(3);
+    } else {
+      age1 = age;
+    }
+
+    service.deal(file, sex, census1, l, h, response, age1, days);
+
+  }
 }
--- src/main/java/com/tz/demo1/utils/Util.java
+++ src/main/java/com/tz/demo1/utils/Util.java
@@ -2,6 +2,7 @@ package com.tz.demo1.utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Comparator;
 
 import com.tz.demo1.entity.Debt;
@@ -41,5 +42,55 @@ public class Util {
 		});
 		return debts;
 		
+	}
+	
+	//判断年龄段
+	public static boolean judgeId(String ID,String age){
+	  int len = ID.length();
+      boolean flagAge = false;
+      String[]arr=age.split(",");
+      if(len==18){
+        
+        int arrAge = Integer.parseInt(ID.substring(6, 10));
+        Calendar calendar = Calendar.getInstance();
+        int ages = calendar.get(Calendar.YEAR) - arrAge;
+        for(String s:arr){
+         int first = Integer.parseInt(s.split("-")[0]);
+          int second = Integer.parseInt(s.split("-")[1]);
+          if (ages <=second && ages >= first) {
+            flagAge = true;
+            break;
+            
+          }
+        }
+      }
+     
+      return flagAge;
+	}
+	public static boolean judgeTime(String days,String days1){
+	  /*
+       * 时间段判断
+       */
+      // 表中时间
+      String[] arr = days1.split("/");
+      int year = Integer.parseInt(arr[0]);
+      int month = Integer.parseInt(arr[1]);
+      // 选定的时间
+      int year1 = Integer.parseInt(days.split("/")[0]);
+      int month1 = Integer.parseInt(days.split("/")[1]);
+      boolean flagDays = (year == year1 && month >= month1 && month <= (month1 + 5));
+      return flagDays;
+	}
+	
+	public static boolean judgeCensus(String census,String hometown){
+	  String[]arr=census.split(",");
+	  boolean flagHometown=false;
+	  for(String s:arr){
+	    if(s.equals(hometown)){
+	      flagHometown=true;
+	      break;
+	    }
+	  }
+	  return flagHometown;
 	}
 }
