090a54e386743392e602b1f9f258fce499e927fe
tz <616148442@qq.com>
2018-12-14 14:31:11 +0800

    打包配置[A]

--- src/main/java/com/tz/demo1/Demo1Application.java
+++ src/main/java/com/tz/demo1/Demo1Application.java
@@ -2,11 +2,17 @@ package com.tz.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Demo1Application {
+public class Demo1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 }
